e98a3595f4b0b92a838dd1aeea2ec15b8151e6b5
Tyler Collins <tjscollins@gmail.com>
2017-03-08 13:28:19 +1000

    Modified directory structure to allow for mocha.opts file and adjusted karma.conf.js and webpack.config to match

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
       }
     },
     coverageReporter: {
-      dir: 'tests/coverage/',
+      dir: 'test/coverage/',
       reporters: [
         {
           type: 'html',
@@ -27,8 +27,8 @@ module.exports = function(config) {
     },
     files: [
       // 'node_modules/jquery/dist/jquery.min.js',
-      'tests/client/**/*.test.js',
-      'tests/client/**/*.test.jsx',
+      'test/client/**/*.test.js',
+      'test/client/**/*.test.jsx',
     ],
     frameworks: [
       'mocha', 'sinon',
@@ -37,10 +37,10 @@ module.exports = function(config) {
       'karma-phantomjs-launcher', 'karma-mocha', 'karma-sourcemap-loader', 'karma-webpack', 'karma-coverage', 'karma-mocha-reporter', 'karma-sinon',
     ],
     preprocessors: {
-      'tests/client/**/*.test.js': [
+      'test/client/**/*.test.js': [
         'webpack', 'sourcemap', //'coverage',
       ],
-      'tests/client/**/*.test.jsx': [
+      'test/client/**/*.test.jsx': [
         'webpack', 'sourcemap', //'coverage',
       ],
     },
