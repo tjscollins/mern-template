0c0523a5a9540ebfe736d9b06629a2e7f9000ad9
Tyler Collins <tjscollins@gmail.com>
2017-04-11 17:22:19 +1000

    Simplified components

--- client/react/react-app.jsx
+++ client/react/react-app.jsx
@@ -1,15 +1,22 @@
 /*----------React----------*/
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
 
 /*----------Redux----------*/
 import {Provider} from 'react-redux';
 import configureStore from 'configureStore';
 
 /*----------Components----------*/
-import Application from 'Application';
+import Index from 'Index';
+import Login from 'Login';
+import Profile from 'Profile';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <Application />
+    <Router history={browserHistory}>
+      <Route path='/' component={Index} />
+      <Route path='login' component={Login} />
+      <Route path='profile' component={Profile} />
+    </Router>
   </Provider>, document.getElementById('react-app'));
